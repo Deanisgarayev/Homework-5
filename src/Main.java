7fc48b4fa3f34a8e00dc68c285044df8475e9fc4
Денис <garaevdenis2019@gmail.com>
2023-02-22 21:00:30 +0500

    Сделоно ДЗ

--- src/Main.java
+++ src/Main.java
@@ -19,21 +19,23 @@ public class Main {
         System.out.println("Задача 2");
         int clientDeviceYear = 2015;
         int clientOS = 1;
-        if (clientOS == 0 && clientDeviceYear > 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        if (clientOS == 1 && clientDeviceYear > 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        }
-        if (clientOS == 0 && clientDeviceYear <= 2015) {
+        if (clientOS == 1) {
+            if (clientDeviceYear <= 2015){
+         System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+                System.out.println("Установите  версию приложения для Android по ссылке");
+            }
+            } else {
+        if (clientDeviceYear <= 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            System.out.println("Установите версию приложения для iOS по ссылке");
         }
+    }
     }
     public static void task3() {
         System.out.println("Задача 3");
-        int year = 2022;
+        int year = 2020;
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println("год является високосным");
         } else {
